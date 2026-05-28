96c00826b5dffd07e75ce11e5934a02e035c89e1
Pranav Dattatraya Pate <pranav.pate@yahoo.co.in>
2026-05-28 20:27:22 +0530

    Created 26_05_2026 this folder and added require code in it and crated Hands_On_Practical_Steps this word file, updated steps in it and also updated README.md file in same folder.

---
+++ 26_05_2026/README.md
@@ -0,0 +1,7 @@
+# Performed below mentioned practical.
+
+# End-to-End Tracing on EKS using AWS ADOT (OpenTelemetry). 
+
+# This includes deploying EKS with Karpenter through Terraform, deploying OpenTelemetry through Terraform, and deploying the AWS Data Plane through Terraform.
+
+# Please check Hands_On_Practical_Steps.docx this word file for steps.
---
+++ 26_05_2026/terraform-code/AWS_Data_Plane_terraform-manifests/c1_versions.tf
@@ -0,0 +1,36 @@
+terraform {
+  # Minimum Terraform CLI version required
+  required_version = ">= 1.12.0"
+
+  # Required providers and version constraints
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 6.0"
+    }   
+  }
+
+  # Remote backend configuration using S3 
+  backend "s3" {
+    bucket         = "tfstate-dev-ap-south-1-5n72px"         
+    key            = "retail-persistent-endpoints/dev/terraform.tfstate"            
+    region         = "ap-south-1"
+    profile        = "pranav-terraform-profile"
+    encrypt        = true
+    use_lockfile   = true
+  }
+}
+
+provider "aws" {
+  # AWS region to use for all resources (from variables)
+  region = var.aws_region
+  profile = "pranav-terraform-profile"
+}
+
+
+# Secondary provider specifically for Cart's DynamoDB table
+provider "aws" {
+  alias  = "west2"
+  region = "us-west-2"
+  profile = "pranav-terraform-profile"
+}
